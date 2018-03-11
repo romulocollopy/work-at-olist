1ffc7772df45eae30eb1e151058a0f211ba89a36
Rômulo Collopy <romulocollopy@gmail.com>
2018-03-11 00:23:14 -0300

    framework initial setup

---
+++ phonebilling/settings/databases.py
@@ -0,0 +1,18 @@
+import decouple
+from dj_database_url import parse as db_parse
+
+from .base import BASE_DIR
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+
+DEFAULT_DATABASE_URL = decouple.config(
+    'DATABASE', default=str(
+        'sqlite:///{}/phonebilling.sqlite'.format(BASE_DIR.parent)
+    )
+)
+
+DATABASES = {
+    'default': db_parse(DEFAULT_DATABASE_URL),
+}

713f170a048d34f7a6145402c110caec73e3df6e
Rômulo Collopy <romulocollopy@gmail.com>
2018-03-17 22:38:27 -0300

    pricing service calculates normal fare prices

---
+++ project/apps/core/services/pricing_service.py
@@ -0,0 +1,81 @@
+from decimal import Decimal
+import datetime
+
+SECONDS_IN_A_DAY = 60 * 60 * 24
+
+
+class PricingService:
+
+    def __init__(self):
+        self.standing_charge = Decimal('0.36')
+        self.normal_minute = Decimal('0.09')
+        self.reduced_minute = Decimal('0.00')
+
+        self.start_normal_fares = datetime.time(6, 0, 0)
+        self.start_reduced_fares = datetime.time(22, 0, 0)
+
+    def get_price(self, call):
+        entire_days = self._get_entire_days_value(call)
+        normal_minutes = self._get_normal_minutes_value(call)
+        reduced_minutes = self._get_reduced_minutes_value(call)
+        return (
+            self.standing_charge +
+            normal_minutes +
+            reduced_minutes +
+            entire_days
+        )
+
+    def _get_normal_minutes_value(self, call):
+        return self._get_normal_minutes(call) * self.normal_minute
+
+    def _get_reduced_minutes_value(self, call):
+        return self._get_reduced_minutes(call) * self.reduced_minute
+
+    def _get_normal_minutes(self, call):
+        call_start = datetime.datetime.combine(
+            datetime.date.min, call.start_timestamp.time()
+        )
+        call_end = datetime.datetime.combine(
+            datetime.date.min,
+            (
+                call_start +
+                datetime.timedelta(minutes=call.duration.seconds // 60)
+            ).time()
+        )
+
+        normal_start = datetime.datetime.combine(
+            datetime.datetime.min,
+            self.start_normal_fares
+        )
+
+        normal_end = datetime.datetime.combine(
+            datetime.datetime.min,
+            self.start_reduced_fares
+        )
+
+        if call_end > call_start:
+            start = max(call_start, normal_start)
+            end = min(call_end, normal_end)
+            return (end - start).seconds // 60
+        else:
+            morning = (call_end - normal_start) if call_end > normal_start \
+                else datetime.timedelta(seconds=0)
+            night = (normal_end - call_start) if normal_end > call_start \
+                else datetime.timedelta(seconds=0)
+            return (morning + night).seconds // 60
+
+    def _get_entire_days_value(self, call):
+        days = call.duration.days
+        normal_hours = (
+            self.start_reduced_fares.hour - self.start_normal_fares.hour)
+        reduced_hours = 24 - normal_hours
+
+        return days * (normal_hours * 60 * self.normal_minute +
+                       reduced_hours * 60 * self.reduced_minute)
+
+    def _get_reduced_minutes(self):
+        """ 
+        Since the value will always be 0, we can implement it when the reduced
+        minutes gets a price other than 0
+        """
+        return 0
---
+++ project/apps/core/tests/test_services/test_pricing_service.py
@@ -0,0 +1,95 @@
+import datetime
+import mock
+from decimal import Decimal
+from django.test import TestCase
+from apps.core.services import PricingService
+
+
+class PricingServiceTestCase(TestCase):
+
+    def setUp(self):
+        self.call = mock.Mock()
+        self.call.duration = datetime.timedelta(seconds=(48 * 60 * 60))
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 15, 35, 22)
+        self.service = PricingService()
+
+    @mock.patch.object(PricingService, '_get_entire_days_value')
+    @mock.patch.object(PricingService, '_get_normal_minutes_value')
+    @mock.patch.object(PricingService, '_get_reduced_minutes_value')
+    def test_get_price(self, reduced_value, normal_value, entire_days):
+        reduced_value.return_value = Decimal('0.5')
+        normal_value.return_value = Decimal('17.8')
+        entire_days.return_value = Decimal('172.80')
+
+        price = self.service.get_price(self.call)
+
+        reduced_value.assert_called_once_with(self.call)
+        normal_value.assert_called_once_with(self.call)
+
+        self.assertEqual(
+            price,
+            self.service.standing_charge +
+            reduced_value() +
+            normal_value() +
+            entire_days()
+        )
+
+    @mock.patch.object(PricingService, '_get_normal_minutes')
+    def test_get_normal_minutes_value(self, normal_minutes):
+        normal_minutes.return_value = 13
+        value = self.service._get_normal_minutes_value(self.call)
+        normal_minutes.assert_called_once_with(self.call)
+        self.assertEqual(value, 13 * self.service.normal_minute)
+
+    @mock.patch.object(PricingService, '_get_reduced_minutes')
+    def test_get_reduced_minutes_value(self, reduced_minutes):
+        reduced_minutes.return_value = 27
+        value = self.service._get_reduced_minutes_value(self.call)
+        reduced_minutes.assert_called_once_with(self.call)
+        self.assertEqual(value, 27 * self.service.reduced_minute)
+
+    def test_get_normal_minutes(self):
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 15, 35, 22)
+        self.call.duration = datetime.timedelta(seconds=120 * 60)
+        minutes = self.service._get_normal_minutes(self.call)
+        self.assertEquals(minutes, 120)
+
+    def test_get_normal_minutes_overnight(self):
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 18, 00, 00)
+        self.call.duration = datetime.timedelta(seconds=13 * 60 * 60)
+        minutes = self.service._get_normal_minutes(self.call)
+        self.assertEquals(minutes, 5 * 60)
+
+    def test_get_normal_minutes_past_mindight(self):
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 18, 00, 00)
+        self.call.duration = datetime.timedelta(seconds=8 * 60 * 60)
+        minutes = self.service._get_normal_minutes(self.call)
+        self.assertEquals(minutes, 4 * 60)
+
+    def test_get_normal_minutes_starting_early(self):
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 3, 00, 00)
+        self.call.duration = datetime.timedelta(seconds=5 * 60 * 60)
+        minutes = self.service._get_normal_minutes(self.call)
+        self.assertEquals(minutes, 2 * 60)
+
+    def test_get_normal_minutes_starting_early_finishing_late(self):
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 5, 00, 00)
+        self.call.duration = datetime.timedelta(seconds=18 * 60 * 60)
+        minutes = self.service._get_normal_minutes(self.call)
+        self.assertEquals(minutes, 16 * 60)
+
+    def test_get_normal_minutes_short_call(self):
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 10, 00, 00)
+        self.call.duration = datetime.timedelta(seconds=0.5 * 60 * 60)
+        minutes = self.service._get_normal_minutes(self.call)
+        self.assertEquals(minutes, 0.5 * 60)
+
+    def test_docs_example(self):
+        self.call.start_timestamp = datetime.datetime(2017, 12, 31, 21, 57, 13)
+        self.call.duration = datetime.timedelta(seconds=13 * 60 + 43)
+        minutes = self.service._get_normal_minutes(self.call)
+        self.assertEquals(minutes, 2)
+
+    def test_get_entire_days_value(self):
+        value = self.service._get_entire_days_value(self.call)
+        self.assertEqual(value, Decimal('172.80'))
